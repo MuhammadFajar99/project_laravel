1985d379289adf70e4ecb1d025c41f7c6924f062
Muhammad Fajar <jareemuhammad9951@gmail.com>
2022-04-30 00:25:18 +0700

    penambahan sidebar

--- resources/views/items/sidebar_admin.blade.php
+++ resources/views/items/sidebar_admin.blade.php
@@ -97,7 +97,7 @@
 				</li><li>
 					<a href='?module=JS Arrays'><i class='fa fa-angle-double-right'></i>JS Arrays[ GALIH ADI PRATAMA]</a>
 				</li><li>
-					<a href='?module=JS Array Methods'><i class='fa fa-angle-double-right'>	   </i>JS Array Methods[ 		]</a>
+					<a href="{{route('jsfajar')}}"><i class='fa fa-angle-double-right'>	   </i>JS MUHAMMAD FAJAR[ MUHAMMAD FAJAR		]</a>
 				</li><li>
 					<a href='?module=JS Array Sort'><i class='fa fa-angle-double-right'>		  </i>JS Array Sort[	]</a>
 				</li><li>
